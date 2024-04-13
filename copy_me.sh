97e18306e8cff099bd02d9320dc6514ba36bcfea
gouf <innocent.zero@gmail.com>
2024-04-13 20:44:35 +0900

    refactor: copy_me.sh bash function

--- copy_me.sh
+++ copy_me.sh
@@ -1,10 +1,16 @@
 # Bash function
 function work_day_per_salary {
   currentDir=$(pwd)
-  month=$1
+  projectDir="$HOME/hiwari"
+  rubyRequire="require_relative 'lib/work_days_per_salary'"
+  targetYear=2024
+  targetMonth=$1
+  amount="600_000" # 税抜き報酬額
+  salaryPerDay="WorkDaysPerSalary.new(date: Date.new($targetYear, ARGV[0].to_i, 1), amount: $amount).salary_per_day"
 
-  cd "$HOME/hiwari"
-  # NOTE: 2024年固定なので、時間経過で年数を更新する必要がある
-  bundle e ruby -e "require_relative 'lib/work_days_per_salary';puts (WorkDaysPerSalary.new(date: Date.new(2024, ARGV[0].to_i, 1), amount: 600_000)).salary_per_day" $month
+  cd "$projectDir"
+  # NOTE: 年数固定なので、時間経過に応じて年数を更新する必要がある
+  bundle e ruby -e "$rubyRequire; puts $salaryPerDay" $targetMonth
+
   cd "$currentDir"
 }
