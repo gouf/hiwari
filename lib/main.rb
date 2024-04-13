3dd65c8dad0d4c9a7015d2329d8ccb0c2951c72d
gouf <innocent.zero@gmail.com>
2024-04-13 19:59:12 +0900

    spec & impl: 営業日と日割り計算の追加

---
+++ lib/main.rb
@@ -0,0 +1,32 @@
+require 'holiday_japan'
+require 'active_support/all'
+
+class Main
+  attr_reader :target_date, :amount
+
+  # 対象の年月, 月あたりの受け取り報酬額
+  def initialize(date:, amount:)
+    @target_date = date
+    @amount = amount
+  end
+
+  # 営業日 日数
+  def workdays
+    (target_date.beginning_of_month..target_date.end_of_month).count { workday?(_1) }
+  end
+
+  def salary_per_day
+    (amount.to_f / workdays).round.to_i
+  end
+
+  private
+
+  # 土日・祝祭日は休み
+  def holiday?(date)
+    HolidayJapan.check(date) || date.saturday? || date.sunday?
+  end
+
+  def workday?(date)
+    !holiday?(date)
+  end
+end
---
+++ spec/main_spec.rb
@@ -0,0 +1,35 @@
+describe "Main" do
+  context '2024年5月' do
+    let(:amount) { 600_000 }
+    let(:date) { Date.new(2024, 5, 1) }
+    let(:main) { Main.new(date: date, amount: amount) }
+
+    it '平日が21日あること' do
+      expect(main.workdays).to eq 21
+    end
+
+    context '報酬額が60万円/月の場合' do
+      it '1日あたりの報酬が約28,571円であること' do
+        expect(main.workdays).to eq 21
+        expect(main.salary_per_day).to eq 28571
+      end
+    end
+  end
+
+  context '2024年6月' do
+    let(:amount) { 600_000 }
+    let(:date) { Date.new(2024, 6, 1) }
+    let(:main) { Main.new(date: date, amount: amount) }
+
+    it '平日が22日あること' do
+      expect(main.workdays).to eq 20
+    end
+
+    context '報酬額が60万円/月の場合' do
+      it '1日あたりの報酬が約30,000円であること' do
+        expect(main.workdays).to eq 20
+        expect(main.salary_per_day).to eq 30_000
+      end
+    end
+  end
+end
