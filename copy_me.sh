f19ed2e0d245ea6b168165069e939676890eae7b
gouf <innocent.zero@gmail.com>
2024-04-13 20:53:08 +0900

    Bash script の説明コメントを追記

--- copy_me.sh
+++ copy_me.sh
@@ -1,11 +1,11 @@
 # Bash function
 function work_day_per_salary {
   currentDir=$(pwd)
-  projectDir="$HOME/hiwari"
+  projectDir="$HOME/hiwari" # hiwari があるディレクトリを指定
   rubyRequire="require_relative 'lib/work_days_per_salary'"
-  targetYear=2024
-  targetMonth=$1
-  amount="600_000" # 税抜き報酬額
+  targetYear=2024 # 今年の年数を指定
+  targetMonth=$1 # コマンドで渡す対象の月数
+  amount="600_000" # 税抜き報酬額を指定
   salaryPerDay="WorkDaysPerSalary.new(date: Date.new($targetYear, ARGV[0].to_i, 1), amount: $amount).salary_per_day"
 
   cd "$projectDir"
